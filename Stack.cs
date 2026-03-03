eee34f0c93b22498c7fcb1c7f31497238bc9d2f5
Loran Mariia <mariia.loran.25@.kse.org.ua>
2026-03-03 20:37:01 +0200

    Add class of Stack

---
+++ Stack.cs
@@ -0,0 +1,31 @@
+﻿namespace DefaultNamespace;
+
+public class Stack
+{
+    private int size = 50;
+
+    private string[] _array new string[size] ;
+    private int _сount;
+
+    public void Push(string value)
+    {
+        if (_count == _array.Length )
+        {
+            throw new Exception("Stack is overflow")
+        }
+        _array[_count] = value;
+        _count++
+    }
+
+    public string Pop()
+    {
+        if (_count == 0)
+        {
+            throw new Exception("Stack is empty")
+        }
+
+        var value = _array[_count]
+        _count--
+        return value;
+    }
+}
