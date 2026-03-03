dc5fab39e9f0947b25abd658f0b62fbe1ffb525e
Loran Mariia <mariia.loran.25@.kse.org.ua>
2026-03-03 21:43:05 +0200

    Add class of ArrayList

---
+++ ArrayList.cs
@@ -0,0 +1,44 @@
+﻿namespace DefaultNamespace;
+
+public class ArrayList
+{
+    private int[] _array = new int[10];
+    private  int _count =0;
+
+    public void Add(int value)
+    {
+        _array[_count] = value;
+        _count += 1;
+        if (_count == _array.Length)
+        {
+            var extendedArray = new int[_array.Length * 2];
+            for (var i, i < _array.Length, i++)
+            {
+                extendedArray[i] = _array[i];
+            }
+            _array = extendedArray;
+        }
+    }
+
+    public void Remove(int numer)
+    {
+        for(int i = 0, i < _count, i++) 
+        {
+            if (_array[i] == numer)
+            {
+                for (int j = i; j < _count -1; j++) 
+                 {
+                    _array[j] = _array[j+1]
+                }
+
+                _count _ = 1;
+                return;
+            } 
+        }
+    }
+
+    public int Count()
+    {
+        return _count;
+    }
+}
